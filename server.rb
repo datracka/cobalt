eb61be4ea5407559ff1431da1255eced274399c7
Behsaad Ramez <beh.ramez@gmail.com>
2020-01-19 12:47:36 +0100

    update debugging tools

--- server.rb
+++ server.rb
@@ -38,18 +38,53 @@ end
 get '/so_callback' do
   session_code = request.env['rack.request.query_hash']['code']
   print session_code
-  # ... and POST it back to StackOverflow
-  result = RestClient.post('https://stackoverflow.com/oauth/access_token',
-                          {:client_id => SO_CLIENT_ID,
-                            :client_secret => SO_SECRET,
-                            :code => session_code},
-                            :accept => :json)
 
+
+  payload = { 
+    :client_id => SO_CLIENT_ID,
+    :client_secret => SO_SECRET,
+    :redirect_uri => "localhost:4567"
+  }
+
+  url = 'https://stackoverflow.com/oauth/access_token'
+  result = rest_client_post_request(url, payload)
   # extract the token and granted scopes
-  access_token = JSON.parse(result)['access_token']
+  access_token = result.second['access_token']
 
   print '#######'
   print JSON.parse(result)
   print access_token
   print '#######'
+end
+
+def rest_client_post_request(url, payload, access_token: nil)
+  begin
+    RestClient.log = 'stdout'
+
+    headers = { :accept => :json, content_type: :json }
+
+    unless access_token.nil?
+      headers[:authorization] = "Bearer #{access_token}" 
+    end
+
+    json_obj = JSON.generate(payload)
+    response = RestClient::Request.new({
+      method: :post,
+      url: url,
+      payload: json_obj,
+      headers: { :accept => :json, content_type: :json }
+    }).execute do |response, request, result|
+      case response.code
+      when 400
+        [ :error, JSON.parse(response.to_str) ]
+      when 200
+        [ :success, JSON.parse(response.to_str) ]
+      else
+        fail "Invalid response #{response.to_str} received."
+      end
+    end
+  rescue RestClient::Exception => e
+    Rails.logger.error("error: #{e.message} response: #{e.response}")
+    add_error("error: #{e.message} response: #{e.response}")
+  end
 end
